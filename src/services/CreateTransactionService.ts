6ec6ce2643fac3e54815df4c6b9aa9727fc082d5
Magno Belloni <bellonidesouza6@gmail.com>
2020-06-26 16:14:30 -0300

    Resolução do desafio.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error("Type of transaction must be 'income' or 'outcome'");
+    }
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Outcome value is greater than the total in account!!');
+    }
+
+    const transactionResponse = this.transactionsRepository.create(transaction);
+
+    return transactionResponse;
   }
 }
 
